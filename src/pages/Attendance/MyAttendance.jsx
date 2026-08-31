35d352c83fdf366f783f6d20f6160fd9f346477e
a0m3 <almosawi0309@gmail.com>
2026-08-31 11:09:29 +0300

    formatTime function in my attendance page created

--- src/pages/Attendance/MyAttendance.jsx
+++ src/pages/Attendance/MyAttendance.jsx
@@ -8,3 +8,7 @@ function formatDate(value){
     return new Date(value).toLocaleDateString()
 }
 
+function formatTime(value){
+    if(!value) return "-"
+    return new Date(value).toLocaleTimeString([], {hour: "2-digit", minute:"2-digit"})
+}
