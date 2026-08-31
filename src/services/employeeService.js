3b891399b8e3872d4c09dba9808e9e61335ec017
mohasa7an22 <mohasar7an22@gmail.com>
2026-08-31 10:07:56 +0300

    finished the employee service file

--- src/services/employeeService.js
+++ src/services/employeeService.js
@@ -22,3 +22,26 @@ async function getMyProfile() {
     const response = await api.get(`/employees/me/profile`)
     return response.data
 }
+async function updateMyContact (formData) {
+    const response = await api.patch(`/employees/me/contact`, formData)
+    return response.data
+}
+async function getTeam(){
+    const response = await api.get(`/employees/team`)
+    return response.data
+}
+
+async function getDepartment(){
+    const response = await api.get(`/employees/department`)
+    return response.data
+}
+export {
+    getAllEmployees,
+    getEmployeeById,
+    updateEmployee,
+    updateEmployeeStatus,
+    getMyProfile,
+    updateMyContact,
+    getTeam,
+    getDepartment
+}
